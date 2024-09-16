9c354f28411c86313bf4708dba661bb5fcbefb14
AchroDev <achrodev@proton.me>
2024-09-16 00:14:57 -0500

    Made class Player public and set values for the scenario

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 // Class with the name of 'Player'
 class Player
 {
+// defines the data in the class as public so objects outside of the class can read them.
+public:
     // int x and y for storing player position
     int x, y;
     // int speed for storing player speed
@@ -13,8 +15,10 @@ class Player
 
 int main()
 {
-    // Using the 'Player' class we create a 'Player' variable with the type 'player'
-    Player player;
+    Player player; // Using the 'Player' class we create a 'Player' variable with the type 'player'
+    player.x = 5; // setting a value for the 'x' variable
+    player.y = 6; // setting a value for the 'y' variable
+    player.speed = 1; // setting a value for the 'speed' variable
     
     LOG("This is going to be about classes!");
     std::cin.get();
