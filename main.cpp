c5c0ae0e057f35c240ba49e8da5123a8a2262a44
AchroDev <achrodev@proton.me>
2024-09-16 00:09:41 -0500

    Added Class 'Player' and refactored code to use it

--- main.cpp
+++ main.cpp
@@ -2,7 +2,20 @@
 
 #define LOG(x) std::cout << x << std::endl;
 
+// Class with the name of 'Player'
+class Player
+{
+    // int x and y for storing player position
+    int x, y;
+    // int speed for storing player speed
+    int speed;
+};
+
 int main()
 {
+    // Using the 'Player' class we create a 'Player' variable with the type 'player'
+    Player player;
+    
     LOG("This is going to be about classes!");
+    std::cin.get();
 }
