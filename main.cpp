36e451b992d1e64bddc2219a4bad896cf1350425
AchroDev <achrodev@proton.me>
2024-09-16 00:19:38 -0500

    Added Move function and call in entry point

--- main.cpp
+++ main.cpp
@@ -13,12 +13,23 @@ public:
     int speed;
 };
 
+// Move function using a reference of the Player class
+void Move(Player& player, int xa, int ya)
+{
+    // Updates the players x variable relative to the speed
+    player.x += xa * player.speed;
+    // Updates the players y variable relative to the speed
+    player.y += ya * player.speed;
+}
+
 int main()
 {
     Player player; // Using the 'Player' class we create a 'Player' variable with the type 'player'
     player.x = 5; // setting a value for the 'x' variable
     player.y = 6; // setting a value for the 'y' variable
     player.speed = 1; // setting a value for the 'speed' variable
+    
+    Move(player, 1, -1); // move the player +1 x and -1 y
     
     LOG("This is going to be about classes!");
     std::cin.get();
